9fcac9105f86b7f1f70337fde230825378374a13
Matt Belesiu <matt.belesiu@gmail.com>
2021-11-17 16:21:30 -0800

    taking namespaces and writing checks... wait.. what?

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,5 @@
-# from django import template
-from django.http import HttpResponse, Http404
-# from django.template import loader
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
@@ -14,11 +12,10 @@ def index(request):
 
 
 def detail(request, question_id):
-  try:
-    question = Question.objects.get(pk=question_id)
-  except:
-    raise Http404("Question does not exist")
-  return render(request, 'polls/detail.html', {'question': question})
+
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
 
 def results(request, question_id):
     response = "You're looking at the results for question %s."
